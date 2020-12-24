6664a128974c4a8a70e7351fe3cd63c37da43bb9
João Pedro Lopes <62584207+JohnWalker-117@users.noreply.github.com>
2020-12-24 16:37:32 -0300

    Code
    Arquivo .py

---
+++ StringGenerator.py
@@ -0,0 +1,54 @@
+import string
+import itertools
+import os
+
+caminho = os.environ['HOMEPATH']
+diretorio = 'C:' + caminho + '\\Desktop\\'
+file = diretorio + 'Strings.txt'
+
+ch = string.ascii_letters + string.digits + string.punctuation
+arq = open(file, 'w+')
+
+def generate(maxChar, limite, ch): 
+
+    estado = True
+    while estado:
+        for s in itertools.combinations_with_replacement(ch, maxChar):
+            i = ''.join(s)
+            yield i
+        maxChar += 1
+        if maxChar > limite:
+            arq.close()
+            print('Saindo')
+            estado = False
+            os.sys.exit()
+
+def main():
+
+    maxChar = int(input('Número de caracteres mínimo: \n'))
+    limite = int(input('Número de caracteres máximo: \n'))
+    
+    tipo = input('Tipo de combinação: \n'
+                 ' Letras (1) \n'
+                 ' Números (2) \n'
+                 ' Letras e Números (3) \n'
+                 ' Letras, Números e Símbolos (4) \n')
+    
+    if tipo == '1':
+        ch = string.ascii_letters + 'ç' + 'Ç'
+        
+    elif tipo == '2':
+        ch = string.digits
+        
+    elif tipo == '3':
+        ch = string.ascii_letters + string.digits + 'ç' + 'Ç'
+        
+    elif tipo == '4':
+        ch = string.ascii_letters + string.digits + string.punctuation + 'ç' + 'Ç'
+    
+    for i in generate(maxChar, limite, ch):
+        arq.write(f"{i} ")
+
+if __name__ == '__main__':
+    main()
+    
